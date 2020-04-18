d7e094049900d1facacb2712d74437a1a530d0b3
Mr.Sky1001 <nikita.nk16@yandex.ru>
2020-04-18 16:11:15 +0600

    add style to test

--- src/views/Test/Test.vue
+++ src/views/Test/Test.vue
@@ -1,11 +1,11 @@
 <template>
-    <v-container fluid grid-list-md>
+    <v-container class="test" fluid grid-list-md>
         <v-layout row wrap>
             <v-flex v-for="question in test.questions" :key="question.id">
-                <v-card min-width="400">
+                <v-card class="test-v-card" min-width="500">
                     <v-img class="font-weight-medium black--text align-end" :src="question.image"> </v-img>
                     <v-card-title>{{ question.content }}</v-card-title>
-
+                    <vuetify-audio class="test-hide-shadow" :file="question.audio" color="success"></vuetify-audio>
                     <v-card-actions>
                         <v-radio-group>
                             <v-radio :label="answer.content" v-for="answer in question.answers" :key="answer.number"> </v-radio>
@@ -18,8 +18,12 @@
 </template>
 
 <script>
+import "./_Test.scss"
+import VuetifyAudio from "vuetify-audio"
+
 export default {
     name: "Test",
+    components: { VuetifyAudio },
     computed: {
         test() {
             console.log(this.$store.state)
